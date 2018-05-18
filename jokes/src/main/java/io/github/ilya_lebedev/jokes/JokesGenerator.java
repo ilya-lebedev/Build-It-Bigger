8fdd9dfbf2afeb1579563f8dd2e3d0af63406e75
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-05-18 09:59:18 +0300

    feat: Add dummy implementation of jokes generator

--- jokes/src/main/java/io/github/ilya_lebedev/jokes/JokesGenerator.java
+++ jokes/src/main/java/io/github/ilya_lebedev/jokes/JokesGenerator.java
@@ -1,4 +1,13 @@
 package io.github.ilya_lebedev.jokes;
 
 public class JokesGenerator {
+
+    private int jokeNumber;
+
+    public String getNextJoke() {
+        jokeNumber++;
+        
+        return "Very funny joke #" + jokeNumber;
+    }
+
 }
